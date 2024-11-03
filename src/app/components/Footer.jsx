28acd400b1c0fdce78d9e1f50d0de626574df60b
Mohammed Shamil <kmshamil123@gmail.com>
2024-11-03 19:30:43 +0530

    Updated footer name

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span className="font-bold">S7xdd</span>
+        <span className="font-bold">Mohammed Shamil</span>
         <p className="text-slate-600">2024 All rights reserved.</p>
       </div>
     </footer>
